f6799e9f3955aa89311ca35f679ad7275811fa21
Денис Филлер <denis.filler@yandex.ru>
2023-03-04 18:22:37 +0300

    fixed errors in func group_posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('Main Page')
 
 
-def group_posts():
-    return HttpResponse("Group's Posts")
+def group_posts(request, slug):
+    return HttpResponse(f'Post {slug}')
