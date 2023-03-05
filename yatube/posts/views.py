01731f6f022e0d03d48772898b09d7f261279859
Денис Филлер <denis.filler@yandex.ru>
2023-03-05 15:40:27 +0300

    added render() + index.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Main Page')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
