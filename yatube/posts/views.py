373305ec2150aebde578e3434330c1a380144648
Денис Филлер <denis.filler@yandex.ru>
2023-03-05 15:43:38 +0300

    added docstrings to funcs

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Возвращает отображение главной страницы"""
     template = 'posts/index.html'
     return render(request, template)
 
 
 def group_posts(request, slug):
+    """Возвращает отображение постов в группах"""
     return HttpResponse(f'Post {slug}')
